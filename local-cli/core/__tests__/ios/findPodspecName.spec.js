e8daf817932b4983cfaac9b640b00e5197bd8d89
Miron Pawlik <miron.pawlik@swmansion.com>
2017-10-13 12:54:28 +0200

    Update tests to remove `mock-fs` dependency.

--- local-cli/core/__tests__/ios/findPodspecName.spec.js
+++ local-cli/core/__tests__/ios/findPodspecName.spec.js
@@ -1,23 +1,25 @@
 'use strict';
 
+jest.mock('fs');
+
 const findPodspecName = require('../../ios/findPodspecName');
-const mockFS = require('mock-fs');
+const fs = require('fs');
 const projects = require('../../__fixtures__/projects');
 const ios = require('../../__fixtures__/ios');
 
 describe('ios::findPodspecName', () => {
   it('returns null if there is not podspec file', () => {
-    mockFS(projects.flat);
+    fs.__setMockFilesystem(projects.flat);
     expect(findPodspecName('')).toBeNull();
   });
 
   it('returns podspec name if only one exists', () => {
-    mockFS(ios.pod);
-    expect(findPodspecName('')).toBe('TestPod');
+    fs.__setMockFilesystem(ios.pod);
+    expect(findPodspecName('/')).toBe('TestPod');
   });
 
   it('returns podspec name that match packet directory', () => {
-    mockFS({
+    fs.__setMockFilesystem({
       user: {
         PacketName: {
           'Another.podspec': 'empty',
@@ -25,11 +27,11 @@ describe('ios::findPodspecName', () => {
         }
       }
     });
-    expect(findPodspecName('user/PacketName')).toBe('PacketName');
+    expect(findPodspecName('/user/PacketName')).toBe('PacketName');
   });
 
   it('returns first podspec name if not match in directory', () => {
-    mockFS({
+    fs.__setMockFilesystem({
       user: {
         packet: {
           'Another.podspec': 'empty',
@@ -37,20 +39,29 @@ describe('ios::findPodspecName', () => {
         }
       }
     });
-    expect(findPodspecName('user/packet')).toBe('Another');
+    expect(findPodspecName('/user/packet')).toBe('Another');
   });
 
   it('returns name of podspec generated in ios directory', () => {
-    mockFS({
+    fs.__setMockFilesystem({
       ios: {
         'generated.podspec': 'empty',
       }
     });
-    expect(findPodspecName('node_module/generated')).toBe('generated');
+    //this test requires some additional mocking - fs mock prefer absolute paths
+    const orignalCwd = process.cwd;
+    Object.defineProperty(process, 'cwd', {  
+      value: () => '/'
+    });
+    
+    try {
+      expect(findPodspecName('node_module/generated')).toBe('generated');
+    }
+    catch(err) {
+      Object.defineProperty(process, 'cwd', {  
+        value: () => this.orignalCwd
+      });
+      throw err;
+    }
   });
-
-  afterEach(() => {
-    mockFS.restore();
-  });
-
 });
